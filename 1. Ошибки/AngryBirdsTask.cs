00d3a89d74be6ce75ac8e76990ed655ace92cc2e
Denis Chernov <naantarktide@gmail.com>
2021-08-09 16:51:58 +0700

    add Angry Birds

---
+++ 1. Ошибки/AngryBirdsTask.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace AngryBirds
+{
+	public static class AngryBirdsTask
+	{
+		public static double FindSightAngle(double v, double distance)
+		{
+			const double G = 9.8;
+			double sinAlfa = (distance * G) / (Math.Pow(v, 2));
+			double angle = 0.5 * Math.Asin(sinAlfa);
+			return angle;
+		}
+	}
+}
