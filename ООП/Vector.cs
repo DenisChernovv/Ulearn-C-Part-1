86a6c1829a6050c5eb290a41e6f3c11cb0f33ffb
Denis Chernov <naantarktide@gmail.com>
2021-08-11 18:58:34 +0700

    add Vector

---
+++ ООП/Vector.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Vektor
+{
+    namespace GeometryTasks
+    {
+        public class Vector
+        {
+            public double X;
+            public double Y;
+        }
+
+        public class Geometry
+        {
+            static public double GetLength(Vector vec)
+            {
+                double length = Math.Sqrt((vec.X * vec.X + vec.Y * vec.Y));
+                return length;
+            }
+
+            static public Vector Add(Vector vectorOne, Vector vectorTwo)
+            {
+                var sumVector = new Vector();
+                sumVector.X = vectorOne.X + vectorTwo.X;
+                sumVector.Y = vectorOne.Y + vectorTwo.Y;
+                return sumVector;
+            }
+        }
+    }
+}
