5fb42aa08956e4599fae69d08fac27b81dcd48e7
Denis Chernov <naantarktide@gmail.com>
2021-08-25 21:24:28 +0700

    removedFalseReadOnlyTaskAddTry

--- 13. Целостность данных/ReadOnlyVectorTask.cs
+++ 13. Целостность данных/ReadOnlyVectorTask.cs
@@ -4,75 +4,31 @@ namespace ReadOnlyVectorTask
 {
 	public class ReadOnlyVector
 	{
-		public double X;
-		public double Y;
+		public readonly double X;
+		public readonly double Y;
 
-		public double GetLength()
+		public ReadOnlyVector(double x, double y)
 		{
-			return Geometry.GetLength(this);
+			X = x;
+			Y = y;
 		}
 
-		public ReadOnlyVector Add(ReadOnlyVector vector)
+		public ReadOnlyVector Add(ReadOnlyVector other)
 		{
-			return Geometry.Add(this, vector);
-		}
-
-		public bool Belongs(Segment segment)
-		{
-			return Geometry.IsVectorInSegment(this, segment);
-		}
-	}
-
-	public class Geometry
-	{
-		static public double GetLength(ReadOnlyVector vec)
-		{
-			double length = Math.Sqrt(vec.X * vec.X + vec.Y * vec.Y);
-			return length;
-		}
-
-		static public double GetLength(Segment segment)
-		{
-			var length = Math.Sqrt((segment.End.X - segment.Begin.X) * (segment.End.X - segment.Begin.X)
-				+ (segment.End.Y - segment.Begin.Y) * (segment.End.Y - segment.Begin.Y));
-			return length;
-		}
-
-		static public bool IsVectorInSegment(ReadOnlyVector point, Segment segment)
-		{
-			bool isPointOnSegment = false;
-			var segmentBeginToPoint = new Segment { Begin = segment.Begin, End = point };
-			var segmentPointToEnd = new Segment { Begin = point, End = segment.End };
-
-			var lengthSegment = Geometry.GetLength(segment);
-			var lengthBeginPoint = Geometry.GetLength(segmentBeginToPoint);
-			var lengthPointEnd = Geometry.GetLength(segmentPointToEnd);
-
-			if (lengthBeginPoint + lengthPointEnd == lengthSegment) isPointOnSegment = true;
-
-			return isPointOnSegment;
-		}
-
-		static public ReadOnlyVector Add(ReadOnlyVector vectorOne, ReadOnlyVector vectorTwo)
-		{
-			var sumVector = new ReadOnlyVector();
-			sumVector.X = vectorOne.X + vectorTwo.X;
-			sumVector.Y = vectorOne.Y + vectorTwo.Y;
+			double x = this.X + other.X;
+			double y = this.Y + other.Y;
+			var sumVector = new ReadOnlyVector(x, y);
 			return sumVector;
 		}
-	}
 
-	public class Segment
-	{
-		public ReadOnlyVector Begin;
-		public ReadOnlyVector End;
-		public double GetLength()
+		public ReadOnlyVector WithX(double x)
 		{
-			return Geometry.GetLength(this);
+			return new ReadOnlyVector(x, this.Y);
 		}
-		public bool Contains(ReadOnlyVector vector)
+
+		public ReadOnlyVector WithY(double y)
 		{
-			return Geometry.IsVectorInSegment(vector, this);
+			return new ReadOnlyVector(this.X, y);
 		}
 	}
 }
