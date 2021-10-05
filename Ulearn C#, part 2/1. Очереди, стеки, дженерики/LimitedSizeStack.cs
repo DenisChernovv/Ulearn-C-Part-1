9294a168c158edb6edbc0a63db36794e393bf139
Denis Chernov <naantarktide@gmail.com>
2021-10-05 07:59:05 +0700

    addPartTwo

---
+++ Ulearn C#, part 2/1. Очереди, стеки, дженерики/LimitedSizeStack.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoApplication
+{
+    public class LimitedSizeStack<T>
+    {
+        LinkedList<T> List = new LinkedList <T>();
+        private int lim;
+
+        public LimitedSizeStack(int limit)
+        {
+            lim = limit;
+        }
+
+        public void Push(T item)
+        {
+            List.AddLast(item);
+            if (List.Count > lim)
+                List.RemoveFirst();
+        }
+
+        public T Pop()
+        {
+            if (List.Count == 0) throw new NotImplementedException();
+            var item = List.Last.Value;
+            List.RemoveLast();
+            return item;
+        }
+
+        public int Count
+        {
+            get
+            {
+                return List.Count;
+            }
+        }
+    }
+}
---
+++ Ulearn C#, part 2/1. Очереди, стеки, дженерики/ListModel.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TodoApplication
+{
+    public class ListModel<TItem>
+    {
+        public List<TItem> Items { get; }
+        LimitedSizeStack<Tuple<Operations, TItem, int>> LimidetStack;
+        enum Operations { Add, Remove };
+
+        public ListModel(int limit)
+        {
+            Items = new List<TItem>();
+            LimidetStack = new LimitedSizeStack<Tuple<Operations, TItem, int>>(limit);
+        }
+
+        public void AddItem(TItem item)
+        {
+            Items.Add(item);
+            var tuple = new Tuple<Operations, TItem, int>(Operations.Add,
+                item, Items.Count - 1);
+            LimidetStack.Push(tuple);
+        }
+
+        public void RemoveItem(int index)
+        {
+            var item = Items[index];
+            Items.RemoveAt(index);
+            var tuple = new Tuple<Operations, TItem, int>(Operations.Remove,
+                item, index);
+            LimidetStack.Push(tuple);
+
+        }
+
+        public bool CanUndo()
+        {
+            return LimidetStack.Count != 0;
+        }
+
+        public void Undo()
+        {
+            var tuple = LimidetStack.Pop();
+            switch (tuple.Item1)
+            {
+                case Operations.Add:
+                    Items.RemoveAt(tuple.Item3);
+                    return;
+                case Operations.Remove:
+                    Items.Insert(tuple.Item3, tuple.Item2);
+                    return;
+            }
+
+            throw new NotImplementedException();
+        }
+    }
+}
